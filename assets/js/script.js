e89308fe09e9af1b70d2f3d8a2a3e91f5a50b1a7
Noah Manno <noahmanno19@gmail.com>
2024-03-30 13:24:56 -0400

    functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
+//modal variables and event listener
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// })
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+let tasks = []
 
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
-
-// Todo: create a function to generate a unique task id
+// DONE! // Todo: create a function to generate a unique task id 
 function generateTaskId() {
 
     if (nextId === null || nextId === undefined) {
@@ -24,9 +26,23 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    let todoCards = $('#todo-cards');
+    let formattedDueDate = dayjs(task.dueDate).format('MMM DD, YYYY');
 
+    let card = $('<div>').addClass('card bg-primary');
+    let cardHeader = $('<div>').addClass('card-header').text(task.title);
+    let cardBody = $('<div>').addClass('card-body');
+    let cardTextOne = $('<p>').addClass('card-text').text(task.description);
+    let cardTextTwo = $('<p>').addClass('card-text').text('Due Date: ' + formattedDueDate);
+    let button = $('<a>').addClass('btn btn-primary').text('Delete')
 
+    card.append(cardHeader);
+    card.append(cardBody);
+    cardBody.append(cardTextOne);
+    cardBody.append(cardTextTwo);
+    cardBody.append(button);
 
+    todoCards.append(card);
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -34,9 +50,37 @@ function renderTaskList() {
 
 }
 
+const saveChanges = $('#save-changes')
+
+saveChanges.on('click', function() {
+    handleAddTask()
+})
+
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    
+    let taskTitle = $('#task-title');
+    let taskTitleInput = taskTitle.val();
 
+    let dueDate = $('#datepicker');
+    let dueDateInput = dueDate.val();
+
+    let description = $('#description');
+    let descriptionInput = description.val();
+
+    let task = {
+        title: taskTitleInput, 
+        date: dueDateInput,
+        description: descriptionInput,
+    };
+
+    console.log(task);
+    createTaskCard(task);
+
+    //clear Inputs
+    taskTitle.val('');
+    dueDate.val('');
+    description.val('');
 }
 
 // Todo: create a function to handle deleting a task
